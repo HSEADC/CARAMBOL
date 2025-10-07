5738e10af66d2e98614073a6c62761fa209862c4
Irina Kondratenko <iiiii.irina.irina@gmail.com>
2025-10-07 11:25:46 +0300

    second

--- config/webpack.pages.js
+++ config/webpack.pages.js
@@ -9,14 +9,14 @@ function createPages(template, filename) {
 
 const htmlPages = [
   createPages("./src/index.html", "./index.html"),
-  createPages("./src/pages/articles.html", "./pages/articles.html"),
-  createPages("./src/pages/tests.html", "./pages/tests.html"),
-  createPages("./src/pages/dictionary.html", "./pages/dictionary.html"),
-  createPages(
-    "./src/pages/articles/plants.html",
-    "./pages/articles/plants.html"
-  ),
+  createPages("./src/pages/plants.html", "./pages/plants.html"),
+  createPages("./src/pages/instruction.html", "./pages/instruction.html"),
   createPages("./src/pages/tests/test1.html", "./pages/tests/test1.html"),
+  createPages("./src/pages/stories/story1.html", "./pages/stories/story1.html"),
+  createPages(
+    "./src/pages/articles/article1.html",
+    "./pages/articles/article1.html"
+  ),
 ];
 
 module.exports = htmlPages;
