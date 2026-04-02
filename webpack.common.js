3ae840a563ab647a9ec87cc1d02a05668a45a7de
Ekaterina Demyanets <kot323241@gmail.com>
2026-04-02 20:11:28 +0300

    add build

--- webpack.common.js
+++ webpack.common.js
@@ -47,7 +47,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'docs'),
-    publicPath: '/'
+    publicPath: '/CARAMBOL/'
     // clean: true
   },
   module: {
